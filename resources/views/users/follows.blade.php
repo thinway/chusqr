e3e77ad53596dff8e5d2be4c04c52a29db5a54ca
Fran Delgado <fran@thinway.org>
2018-02-06 11:36:32 +0100

    Mejorada la vista de la lista de usuarios seguidos.

--- resources/views/users/follows.blade.php
+++ resources/views/users/follows.blade.php
@@ -1,9 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    <ul>
-    @foreach($user->follows as $follow)
-        <li><a href="/{{ $follow->slug }}">{{ $follow->name}}</a></li>
+    <div class="grid-x grid-margin-x">
+    @foreach($user->follows->chunk(3) as $row)
+        <div class="small-12 medium-4 cell">
+            @foreach($row as $user)
+                <div class="card" style="">
+                    <div class="card-divider">
+                        {{ $user->name }}
+                    </div>
+                    <img src="https://picsum.photos/200/200/?random">
+                    <div class="card-section">
+                        <h4>&#64;{{ $user->slug }}</h4>
+                        <table class="text-center">
+                            <thead>
+                            <tr>
+                                <th class="text-center"><a href="/{{ $user->slug }}/follows">Following</a></th>
+                                <th class="text-center">Followers</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>{{ count($user->follows) }}</td>
+                                <td>{{ count($user->followers) }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     @endforeach
-    </ul>
+    </div>
 @endsection
